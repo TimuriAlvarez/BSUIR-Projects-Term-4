54a22543ad26226e59da56d8bbb6688f484f8be2
Timuri <timurialvarez@gmail.com>
2022-04-08 21:57:39 +0300

    TLinux update

---
+++ TLinuxInterfaceC/TLinuxInterfaceC.pro
@@ -0,0 +1,12 @@
+TEMPLATE = app
+CONFIG += console
+CONFIG -= app_bundle
+CONFIG -= qt
+
+SOURCES += \
+		*.c
+
+HEADERS += \
+		*.h \
+		sources/*.h \
+		sources/sys/*.h
---
+++ TLinuxInterfaceC/tlinux.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "tlinux/sys/types.h"
+#include "tlinux/sys/wait.h"
+#include "tlinux/dirent.h"
+#include "tlinux/fcntl.h"
+#include "tlinux/signal.h"
+#include "tlinux/termios.h"
+#include "tlinux/time.h"
+#include "tlinux/unistd.h"
